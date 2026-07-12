ada6d6f4b89b6bb761830bce89db28dae73e81b7
rituharsh9436 <rituharsh9436@gmail.com>
2026-07-12 12:03:38 +0530

    added the validity for govt id and adhar numbers.

--- backend/config.py
+++ backend/config.py
@@ -7,6 +7,8 @@ class Settings:
     # This pulls from your .env file
     MONGO_URL = os.getenv("MONGO_URL")
     DATABASE_NAME = "auth_db" 
+    VALID_DEPARTMENT_IDS_COLLECTION = os.getenv("VALID_DEPARTMENT_IDS_COLLECTION", "valid_department_ids")
+    VALID_AADHAAR_NUMBERS_COLLECTION = os.getenv("VALID_AADHAAR_NUMBERS_COLLECTION", "valid_aadhaar_numbers")
     SECRET_KEY = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
